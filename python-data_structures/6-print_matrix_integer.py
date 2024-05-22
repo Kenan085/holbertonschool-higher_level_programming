9b759fa6e124181befe6e176d02b068244429539
Kenan <kenanshafizade85@gmail.com>
2024-05-22 09:34:53 +0000

    Task 6 fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+    for i in range(len(matrix)):
+        lenn = len(matrix[i])
+        for j in range(lenn):
+            print("{:d}".format(matrix[i][j]), end=" ")
+            if j != lenn - 1:
+                print(end=" ")
         print()

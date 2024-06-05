f68f69a5f7537ea73ed54fa0dbf0e65b90aacb0b
Kenan <kenanshafizade85@gmail.com>
2024-06-05 08:55:29 +0000

    2nd

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 
 def read_file(filename=""):
     """A function that reads a text file"""
-    with open ("filename", "r", encoding="utf") as f:
+    with open (filename, "r", encoding="utf8") as f:
         content = f.read()
         for line in f:
             print(line, end='')

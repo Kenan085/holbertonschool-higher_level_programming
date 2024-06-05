b92a7ec88393740a22392054f4a0abff921228f4
Kenan <kenanshafizade85@gmail.com>
2024-06-05 09:16:22 +0000

    Task 1 and Task 2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,7 @@
 
 def write_file(filename="", text=""):
     """A function that writes a string to a text file """
-    with open(filename, 'w', 'utf8') as f:
+    with open(filename, 'w', encoding='utf8') as f:
         content = f.write(text)
         return content
+
